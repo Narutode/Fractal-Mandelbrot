89651d0945dfd3ffc740033f84ac9d9832826c3d
Marc Brgs <66321703+marc-brgs@users.noreply.github.com>
2022-11-10 11:44:46 +0100

    Mandelbrot working
    - ColorSet fixed
    - Render 500x500

--- Factral-Mandelbrot/Assets/Mandelbrot.cs
+++ Factral-Mandelbrot/Assets/Mandelbrot.cs
@@ -24,7 +24,7 @@ public class Mandelbrot : MonoBehaviour
     void Start()
     {
         width = 4.5;
-        height = width * (Screen.height / Screen.width);
+        height = width * Screen.height / Screen.width;
         rStart = -2.0;
         iStart = -1.25;
         zoom = 10;
@@ -95,7 +95,7 @@ public class Mandelbrot : MonoBehaviour
 
     Color SetColor(int value)
     {
-        Vector4 CalcColor = new Vector4(0, 0, 0, 1f);
+        UnityEngine.Vector4 CalcColor = new Vector4(0, 0, 0, 1f);
 
         if (value != maxIterations)
         {
@@ -104,117 +104,119 @@ public class Mandelbrot : MonoBehaviour
             switch (colorNr)
             {
                 case 0:
-                {
-                    CalcColor[0] = 66f / 255f;
-                    CalcColor[1] = 30f / 255f;
-                    CalcColor[2] = 15f / 255f;
-                    break;
-                }
-                case 1:
-                {
-                    CalcColor[0] = 25f / 255f;
-                    CalcColor[1] = 7f / 255f;
-                    CalcColor[2] = 26f / 255f;
-                    break;
-                }
-                case 2:
-                {
-                    CalcColor[0] = 9f / 255f;
-                    CalcColor[1] = 1f / 255f;
-                    CalcColor[2] = 47f / 255f;
-                    break;
-                }
-                case 3:
-                {
-                    CalcColor[0] = 4f / 255f;
-                    CalcColor[1] = 4f / 255f;
-                    CalcColor[2] = 73f / 255f;
-                    break;
-                }
-                case 4:
-                {
-                    CalcColor[0] = 0f / 255f;
-                    CalcColor[1] = 7f / 255f;
-                    CalcColor[2] = 100f / 255f;
-                    break;
-                }
-                case 5:
-                {
-                    CalcColor[0] = 12f / 255f;
-                    CalcColor[1] = 44f / 255f;
-                    CalcColor[2] = 138f / 255f;
-                    break;
-                }
-                case 6:
-                {
-                    CalcColor[0] = 24f / 255f;
-                    CalcColor[1] = 82f / 255f;
-                    CalcColor[2] = 177f / 255f;
-                    break;
-                }
-                case 7:
-                {
-                    CalcColor[0] = 57f / 255f;
-                    CalcColor[1] = 125f / 255f;
-                    CalcColor[2] = 209f / 255f;
-                    break;
-                }
-                case 8:
-                {
-                    CalcColor[0] = 134f / 255f;
-                    CalcColor[1] = 181f / 255f;
-                    CalcColor[2] = 229f / 255f;
-                    break;
-                }
-                case 9:
-                {
-                    CalcColor[0] = 211f / 255f;
-                    CalcColor[1] = 236f / 255f;
-                    CalcColor[2] = 248f / 255f;
-                    break;
-                }
-                case 10:
-                {
-                    CalcColor[0] = 241f / 255f;
-                    CalcColor[1] = 233f / 255f;
-                    CalcColor[2] = 191f / 255f;
-                    break;
-                }
-                case 11:
-                {
-                    CalcColor[0] = 248f / 255f;
-                    CalcColor[1] = 201f / 255f;
-                    CalcColor[2] = 95f / 255f;
-                    break;
-                }
-                case 12:
-                {
-                    CalcColor[0] = 255f / 255f;
-                    CalcColor[1] = 170f / 255f;
-                    CalcColor[2] = 0f / 255f;
-                    break;
-                }
-                case 13:
-                {
-                    CalcColor[0] = 204f / 255f;
-                    CalcColor[1] = 128f / 255f;
-                    CalcColor[2] = 0f / 255f;
-                    break;
-                }
-                case 14:
-                {
-                    CalcColor[0] = 153f / 255f;
-                    CalcColor[1] = 87f / 255f;
-                    CalcColor[2] = 0f / 255f;
-                    break;
-                }
-                case 15:
-                {
-                    CalcColor[0] = 106f / 255f;
-                    CalcColor[1] = 52f / 255f;
-                    CalcColor[2] = 3f / 255f;
-                    break;
-                }
+					{
+						CalcColor[0] = 66.0f / 255.0f;
+						CalcColor[1] = 30.0f / 255.0f;
+						CalcColor[2] = 15.0f / 255.0f;
+
+						break;
+					}
+				case 1:
+					{
+						CalcColor[0] = 25.0f / 255.0f;
+						CalcColor[1] = 7.0f / 255.0f;
+						CalcColor[2] = 26.0f / 255.0f;
+						break;
+					}
+				case 2:
+					{
+						CalcColor[0] = 9.0f / 255.0f;
+						CalcColor[1] = 1.0f / 255.0f;
+						CalcColor[2] = 47.0f / 255.0f;
+						break;
+					}
+
+				case 3:
+					{
+						CalcColor[0] = 4.0f / 255.0f;
+						CalcColor[1] = 4.0f / 255.0f;
+						CalcColor[2] = 73.0f / 255.0f;
+						break;
+					}
+				case 4:
+					{
+						CalcColor[0] = 0.0f / 255.0f;
+						CalcColor[1] = 7.0f / 255.0f;
+						CalcColor[2] = 100.0f / 255.0f;
+						break;
+					}
+				case 5:
+					{
+						CalcColor[0] = 12.0f / 255.0f;
+						CalcColor[1] = 44.0f / 255.0f;
+						CalcColor[2] = 138.0f / 255.0f;
+						break;
+					}
+				case 6:
+					{
+						CalcColor[0] = 24.0f / 255.0f;
+						CalcColor[1] = 82.0f / 255.0f;
+						CalcColor[2] = 177.0f / 255.0f;
+						break;
+					}
+				case 7:
+					{
+						CalcColor[0] = 57.0f / 255.0f;
+						CalcColor[1] = 125.0f / 255.0f;
+						CalcColor[2] = 209.0f / 255.0f;
+						break;
+					}
+				case 8:
+					{
+						CalcColor[0] = 134.0f / 255.0f;
+						CalcColor[1] = 181.0f / 255.0f;
+						CalcColor[2] = 229.0f / 255.0f;
+						break;
+					}
+				case 9:
+					{
+						CalcColor[0] = 211.0f / 255.0f;
+						CalcColor[1] = 236.0f / 255.0f;
+						CalcColor[2] = 248.0f / 255.0f;
+						break;
+					}
+				case 10:
+					{
+						CalcColor[0] = 241.0f / 255.0f;
+						CalcColor[1] = 233.0f / 255.0f;
+						CalcColor[2] = 191.0f / 255.0f;
+						break;
+					}
+				case 11:
+					{
+						CalcColor[0] = 248.0f / 255.0f;
+						CalcColor[1] = 201.0f / 255.0f;
+						CalcColor[2] = 95.0f / 255.0f;
+						break;
+					}
+				case 12:
+					{
+						CalcColor[0] = 255.0f / 255.0f;
+						CalcColor[1] = 170.0f / 255.0f;
+						CalcColor[2] = 0.0f / 255.0f;
+						break;
+					}
+				case 13:
+					{
+						CalcColor[0] = 204.0f / 255.0f;
+						CalcColor[1] = 128.0f / 255.0f;
+						CalcColor[2] = 0.0f / 255.0f;
+						break;
+					}
+				case 14:
+					{
+						CalcColor[0] = 153.0f / 255.0f;
+						CalcColor[1] = 87.0f / 255.0f;
+						CalcColor[2] = 0.0f / 255.0f;
+						break;
+					}
+				case 15:
+					{
+						CalcColor[0] = 106.0f / 255.0f;
+						CalcColor[1] = 52.0f / 255.0f;
+						CalcColor[2] = 3.0f / 255.0f;
+						break;
+					}
             }
         }
 
